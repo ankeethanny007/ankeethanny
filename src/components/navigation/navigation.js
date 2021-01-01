1ba8525c6e3b301ceaec3dbf57dfc689d54b41d2
Ankeet Kumar Hanny <ankeethanny1@gmail.com>
2021-01-02 02:28:26 +0530

    Add files via upload

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome } from '@fortawesome/free-solid-svg-icons'
+import { faHome, faUser, faNewspaper, faCog, faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import './navigation.scss'
 
 class Navigation extends Component {
@@ -12,15 +12,16 @@ class Navigation extends Component {
         }, 50);
     }
     render() {
+        const screenWidth = window.innerWidth;
         return (
             <div>
                 <div id="bottomnav" className="bottomnav" >
                     <ul>
-                        <li className={window.location.pathname === '/about' ? 'active' : ''}><Link to="/about" id="about"> About </Link></li>
-                        <li className={window.location.pathname === '/skills' ? 'active' : ''}><Link to="/skills" id="skills"> Skills </Link></li>
+                        <li className={window.location.pathname === '/about' ? 'active' : ''}><Link to="/about" id="about"> {screenWidth >= 768 ? 'About' : <FontAwesomeIcon icon={faUser} />} </Link></li>
+                        <li className={window.location.pathname === '/skills' ? 'active' : ''}><Link to="/skills" id="skills"> {screenWidth >= 768 ? 'Skills' : <FontAwesomeIcon icon={faCog} />} </Link></li>
                         <li className={window.location.pathname === '/' || window.location.pathname === '/home' ? 'active' : ''}><Link to="/" id="home"> <FontAwesomeIcon icon={faHome} /> </Link></li>
-                        <li className={window.location.pathname === '/blog' ? 'active' : ''}><Link to="/blog" id="blog"> Blog </Link></li>
-                        <li className={window.location.pathname === '/contact' ? 'active' : ''}><Link to="/contact" id="contact"> Contact </Link></li>
+                        <li className={window.location.pathname === '/blog' ? 'active' : ''}><Link to="/blog" id="blog"> {screenWidth >= 768 ? 'Blog' : <FontAwesomeIcon icon={faNewspaper} />}</Link></li>
+                        <li className={window.location.pathname === '/contact' ? 'active' : ''}><Link to="/contact" id="contact"> {screenWidth >= 768 ? 'Contact' : <FontAwesomeIcon icon={faEnvelope} />} </Link></li>
                     </ul>
                 </div>
             </div>
